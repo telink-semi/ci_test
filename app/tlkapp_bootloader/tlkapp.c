6df7560cfe5476cb20c5c7db239c862ef1ad4394
mwchen <mengwei.chen@telink-semi.com>
2024-05-23 11:24:48 +0800

    Update tlkapp.c

--- app/tlkapp_bootloader/tlkapp.c
+++ app/tlkapp_bootloader/tlkapp.c
@@ -1,3 +1,32 @@
+/********************************************************************************************************
+ * @file    tlkapp.c
+ *
+ * @brief   This is the source file for BTBLE SDK
+ *
+ * @author  BTBLE GROUP
+ * @date    2022
+ *
+ * @par     Copyright (c) 2022, Telink Semiconductor (Shanghai) Co., Ltd.
+ *          All rights reserved.
+ *
+ *          The information contained herein is confidential property of Telink
+ *          Semiconductor (Shanghai) Co., Ltd. and is available under the terms
+ *          of Commercial License Agreement between Telink Semiconductor (Shanghai)
+ *          Co., Ltd. and the licensee or the terms described here-in. This heading
+ *          MUST NOT be removed from this file.
+ *
+ *          Licensee shall not delete, modify or alter (or permit any third party to delete, modify, or
+ *          alter) any information contained herein in whole or in part except as expressly authorized
+ *          by Telink semiconductor (shanghai) Co., Ltd. Otherwise, licensee shall be solely responsible
+ *          for any claim to the extent arising out of or relating to such deletion(s), modification(s)
+ *          or alteration(s).
+ *
+ *          Licensees are granted free, non-transferable use of the information in this
+ *          file under Mutual Non-Disclosure Agreement. NO WARRANTY of ANY KIND is provided.
+ *
+ *******************************************************************************************************/
+
+
 #include "tlkapi/tlkapi_stdio.h"
 #include "tlkapp_config.h"
 #include "tlkapp_irq.h"
